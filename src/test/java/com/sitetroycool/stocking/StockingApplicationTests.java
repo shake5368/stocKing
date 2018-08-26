c204602c83a94df1de7689de30c6fc90df64f67d
shake5368 <shake5368@gmail.com>
2018-08-26 17:36:38 +0800

    [stocking] update start

--- src/test/java/com/sitetroycool/stocking/StockingApplicationTests.java
+++ src/test/java/com/sitetroycool/stocking/StockingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stocking.application;
+package com.sitetroycool.stocking;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class StockingApplicationTests {
 
 	@Test
 	public void contextLoads() {
